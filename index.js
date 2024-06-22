ec01acec4b9a016e8312bb0f8a6363b14c67b36b
pjustinianogarcia <158845576+pjustinianogarcia@users.noreply.github.com>
2024-06-22 18:42:51 -0400

    Fixed URI

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const mongoose = require('mongoose');
 const Models = require('./models.js');
 
 //URI
-mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+//mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+mongoose.connect('mongodb://localhost:27017/myflixDB', { useNewUrlParser: true, useUnifiedTopology: true });
 
 //models
 const Movies = Models.Movie;
