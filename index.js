faeaff8b44655d4f06492a150bdcd044369887f7
pjustinianogarcia <158845576+pjustinianogarcia@users.noreply.github.com>
2024-06-23 01:05:37 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const Models = require('./models.js');
 
 //URI
- mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+ mongoose.connect('mongodb+srv://patriciajg683:QcSXtDRtg1ZKX33b@myflixdb.2ayrhib.mongodb.net/?retryWrites=true&w=majority&appName=myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
 
 //mongoose.connect('mongodb://localhost:27017/cfDB', { useNewUrlParser: true, useUnifiedTopology: true });
 
