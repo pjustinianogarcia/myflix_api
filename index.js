20e2b3dd6b668af1b5d746d19957c6386434dd79
pjustinianogarcia <158845576+pjustinianogarcia@users.noreply.github.com>
2024-06-23 01:49:22 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const mongoose = require('mongoose');
 const Models = require('./models.js');
 
 //URI
- mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+ //mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-//mongoose.connect('mongodb://localhost:27017/cfDB', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb://localhost:27017/cfDB', { useNewUrlParser: true, useUnifiedTopology: true });
 
 //models
 const Movies = Models.Movie;
