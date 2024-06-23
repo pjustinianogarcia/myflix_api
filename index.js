63eb4ba7e24773094b1e25423f882f7f07b14af7
pjustinianogarcia <158845576+pjustinianogarcia@users.noreply.github.com>
2024-06-23 01:28:53 -0400

    Update index.js

--- index.js
+++ index.js
@@ -32,23 +32,6 @@ const uuid = require('uuid');
 const { check, validationResult } = require('express-validator');
 
 
-    
-
-// configure CORS
-const cors = require('cors');
-let allowedOrigins = ['http://localhost:1234','https://myflix-api-le1z.onrender.com/'];
-
-app.use(cors({
-  origin: (origin, callback) => {
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-      return callback(new Error(message ), false);
-    }
-    return callback(null, true);
-  }
-}));
-
 
 //parse request with json
 app.use(express.json());
